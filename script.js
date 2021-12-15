bc9b95302a8d5b14d8635af8874c11c07e04e25e
Joshua Cheung <contact.cjoshua@gmail.com>
2021-12-15 13:59:37 -0800

    start working on js

---
+++ script.js
@@ -0,0 +1,8 @@
+class Clock {
+    constructor(element) {
+        this.element = element;
+    }
+}
+
+const clockElement = document.querySelector(".clock");
+const clockObject = new.Clock(clockElement);
