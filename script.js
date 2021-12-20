47c739284df8e258fd44ba4c678d7fb136eea9a4
Joshua Cheung <contact.cjoshua@gmail.com>
2021-12-19 16:46:24 -0800

    writing clock method

--- script.js
+++ script.js
@@ -2,7 +2,27 @@ class Clock {
     constructor(element) {
         this.element = element;
     }
+
+    update(){
+        const time = this.getTime();
+        const formattedMin = time.minute.toString().padStart(2,'0');
+        const clockTime = `${time.hour}:${formattedMin}`;
+        const amPm = time.isAm ? "AM" : "PM";
+        
+    }
+
+    getTime() {
+        const now = new Date();
+
+        return {
+            hour: now.getHours() % 12 || 12,
+            minute: now.getMinutes(),
+            isAm: now.getHours() < 12
+        };
+    }
 }
 
 const clockElement = document.querySelector(".clock");
-const clockObject = new.Clock(clockElement);
+const clockObject = new Clock(clockElement);
+
+clockObject.update();
